584b2537a13e911bb004a08c2c14de221d4dab46
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-01-27 21:16:41 +0900

    add 05

---
+++ 05/source/app.d
@@ -0,0 +1,70 @@
+import std.stdio;
+
+import std.traits;
+import std.range;
+
+auto n_gram(Range)(Range r, size_t n)
+if (isInputRange!(Unqual!Range))  // unqaulはqualifierを外すのでconst(int) -> int ということ
+in
+{
+	assert(n >= 1);
+}
+do
+{
+	// とりあえずInputRangeということで
+	// front, popFront, empty を持ってる
+	static struct Result {
+	private:
+		alias R = Unqual!Range;
+		alias T = ElementType!R;
+		size_t n;
+		T[] buf;
+	public:
+		R source;
+
+		this(R source, size_t n) {
+			this.source = source;
+			this.n = n;
+			this.buf = [];
+			foreach (i; 0..n) {
+				this.buf ~= this.source.front();
+				this.source.popFront();
+			}
+		}
+
+		@property {
+			bool empty() {
+				return this.buf.length < this.n;
+			}
+			auto ref front() {
+				return this.buf;
+			}
+		}
+		void popFront() {
+			this.buf.popFront();
+			if (!this.source.empty) {
+				this.buf ~= this.source.front();
+				this.source.popFront();
+			}
+		}
+	}
+
+	return Result(r, n);
+}
+
+auto answer1() {
+	import std.array;
+	string str = "I am an NLPer";
+	return n_gram(str.split(" "), 2);
+}
+auto answer2() {
+	import std.array;
+	string str = "I am an NLPer";
+	return n_gram(str.split(""), 2);
+}
+
+void main()
+{
+	writeln(answer1());
+	writeln(answer2());
+}
